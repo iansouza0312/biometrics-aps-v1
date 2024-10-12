53bd660af06b75c9897fcfc607f09f29018b5856
Ian Souza <ianfmsouza@gmail.com>
2024-10-12 15:03:03 -0300

    chore: start dashborad screen

--- client/src/screens/(logged-in)/DashboardScreen.tsx
+++ client/src/screens/(logged-in)/DashboardScreen.tsx
@@ -1,7 +1,14 @@
 export function DashboardScreen() {
   return (
-    <div>
-      <h2>dashboard</h2>
+    <div className="p-6 max-w-4xl mx-auto space-y-4">
+      <h1 className="text-2xl font-bold text-center">
+        Registro de Propriedades - Indicadores Especiais
+      </h1>
+      <div className="flex items-center justify-center">
+        <span>ffjhhdshbfds</span>
+        <span>ffjhhdshbfds</span>
+        <span>ffjhhdshbfds</span>
+      </div>
     </div>
   );
 }
