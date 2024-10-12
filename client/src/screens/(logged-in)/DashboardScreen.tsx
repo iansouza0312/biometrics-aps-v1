fae3ddb5e6bea85234c6028b1f4cbd79023a6647
Ian Souza <ianfmsouza@gmail.com>
2024-10-12 15:28:50 -0300

    chore: starter graphics layout

--- client/src/screens/(logged-in)/DashboardScreen.tsx
+++ client/src/screens/(logged-in)/DashboardScreen.tsx
@@ -1,3 +1,65 @@
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Rectangle,
+  XAxis,
+  YAxis,
+} from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "../../components/ui/chart";
+
+export const description =
+  "quatidade de propriedades registradas nas 5 principais capitais do pais";
+
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
+
+const today = new Date();
+const formattedDate = today.toLocaleDateString();
+
 export function DashboardScreen() {
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
@@ -5,9 +67,99 @@ export function DashboardScreen() {
         Registro de Propriedades - Indicadores Especiais
       </h1>
       <div className="flex items-center justify-center">
-        <span>ffjhhdshbfds</span>
-        <span>ffjhhdshbfds</span>
-        <span>ffjhhdshbfds</span>
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              quatidade de propriedades registradas em cada estado
+            </CardTitle>
+            <CardDescription>
+              <h2>Última atualização em: {formattedDate}</h2>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <BarChart accessibilityLayer data={chartData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="browser"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) =>
+                    chartConfig[value as keyof typeof chartConfig]?.label
+                  }
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent hideLabel />}
+                />
+                <Bar
+                  dataKey="visitors"
+                  strokeWidth={2}
+                  radius={8}
+                  activeIndex={2}
+                  activeBar={({ ...props }) => {
+                    return (
+                      <Rectangle
+                        {...props}
+                        fillOpacity={0.8}
+                        stroke={props.payload.fill}
+                        strokeDasharray={4}
+                        strokeDashoffset={4}
+                      />
+                    );
+                  }}
+                />
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col items-start gap-2 text-sm">
+            <div className="leading-none text-muted-foreground">
+              Exibindo total de registros de cada estado
+            </div>
+          </CardFooter>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Bar Chart - Mixed</CardTitle>
+            <CardDescription>January - June 2024</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <BarChart
+                accessibilityLayer
+                data={chartData}
+                layout="vertical"
+                margin={{
+                  left: 0,
+                }}
+              >
+                <YAxis
+                  dataKey="browser"
+                  type="category"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) =>
+                    chartConfig[value as keyof typeof chartConfig]?.label
+                  }
+                />
+                <XAxis dataKey="visitors" type="number" hide />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent hideLabel />}
+                />
+                <Bar dataKey="visitors" layout="vertical" radius={5} />
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col items-start gap-2 text-sm">
+            <div className="leading-none text-muted-foreground">
+              Showing total visitors for the last 6 months
+            </div>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
